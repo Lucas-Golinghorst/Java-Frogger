331115d8aa80406dd1c28ed09781959cc699ed0f
Lucas-Golinghorst <golinlg@iastate.edu>
2017-08-08 09:53:11 -0500

    added stddraw class

--- src/Frogger.java
+++ src/Frogger.java
@@ -1,9 +1,6 @@
-import java.awt.event.KeyEvent;
-import java.awt.Color;
-import java.util.ArrayList;
-
 public class Frogger {
     public static void main(String[] args) {
+        StdDraw stdDraw = new StdDraw();
     }
 
 }
